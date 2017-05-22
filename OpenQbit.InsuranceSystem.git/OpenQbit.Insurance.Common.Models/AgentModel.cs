47bd581b3413021be81767ca6249b1a97213ca52
Appuhamy <chaminduilshan@gmail.com>
2017-05-22 17:22:54 +0530

    01-Creating 1,2 Models

--- OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Common.Models/AgentModel.cs
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Common.Models/AgentModel.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace OpenQbit.Insurance.Common.Models
 {
-    class AgentModel
+    public class AgentModel
     {
+        public int ID { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public DateTime Date_of_Birth { get; set; }
+        public string Email { get; set; }
+        public int Mobile { get; set; }
+        public int Telephone { get; set; }
+
     }
 }
