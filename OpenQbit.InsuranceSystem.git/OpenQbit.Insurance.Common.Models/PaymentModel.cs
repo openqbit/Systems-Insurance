8cb829d1981153e4008a0eddaba656bbe07df7a5
Pahansith Gunathilake <pahansith.wsg@gmail.com>
2017-05-23 13:11:07 +0530

    Added model Classes

---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Common.Models/PaymentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Insurance.Common.Models
+{
+    public class PaymentModel
+    {
+        public int ID { get; set; }
+
+    }
+}
---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Common.Models/PolicyCoverageDetailModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Insurance.Common.Models
+{
+    public class PolicyCoverageDetailModel
+    {
+        public int ID { get; set; }
+        public double CoverageValue { get; set; }
+        public double TotalPolicyValue { get; set; }
+
+        public int InsuranceID { get; set; }
+        public int CoverageID { get; set; }
+
+        public virtual ICollection<InsuranceModel> InsuranceList { get; set; }
+        public virtual ICollection<CoverageModel> CoverageList { get; set; }
+
+    }
+}
