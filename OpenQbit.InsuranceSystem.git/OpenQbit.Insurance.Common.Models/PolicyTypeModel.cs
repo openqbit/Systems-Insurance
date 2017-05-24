e17c49cc8e9676518798f2b86a33d5eb989d25ee
sandali_thissera <pst.ames1@gmail.com>
2017-05-24 07:26:01 +0530

    01Test

---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Common.Models/PolicyTypeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Insurance.Common.Models
+{
+    public class PolicyTypeModel
+    {
+        public int ID { get; set; }
+        public String Type { get; set; }
+        public String Eligibilites { get; set; }
+    }
+}
