2a021a7a7b47f83fb5d08856d7c8d4d48128c853
Appuhamy <chaminduilshan@gmail.com>
2017-05-24 15:38:11 +0530

    01-Created Service and API Models

---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Service.WebApi/Controllers/API/PolicyCoverageDetailController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OpenQbit.Insurance.Service.WebApi.Models.API;
+
+namespace OpenQbit.Insurance.Service.WebApi.Controllers.API
+{
+    public class PolicyCoverageDetailController : ApiController
+    {
+        public bool Add(ApiPolicyCoverageDetailModel client)
+        {
+            return true;
+        }
+
+        public bool Update(ApiPolicyCoverageDetailModel client)
+        {
+            return true;
+        }
+
+        public ApiPolicyCoverageDetailModel Get(int? ID)
+        {
+            ApiPolicyCoverageDetailModel pcdm = new ApiPolicyCoverageDetailModel
+            {
+                ID = (int)ID,
+                CoverageValue = 10000,
+                TotalPolicyValue = 100000,
+                Note = "Please Note That",
+                InsuranceID = 1,
+                CoverageID =1,
+            };
+
+            return pcdm;
+        }
+
+        public List<ApiPolicyCoverageDetailModel> GetList()
+        {
+            List<ApiPolicyCoverageDetailModel> pcdmList = new List<ApiPolicyCoverageDetailModel>();
+            ApiPolicyCoverageDetailModel pcdm1 = new ApiPolicyCoverageDetailModel
+            {
+                ID = 1,
+                CoverageValue = 10000,
+                TotalPolicyValue = 100000,
+                Note = "Please Note That",
+                InsuranceID = 1,
+                CoverageID = 1,
+            };
+            pcdmList.Add(pcdm1);
+            ApiPolicyCoverageDetailModel pcdm2 = new ApiPolicyCoverageDetailModel
+            {
+                ID = 2,
+                CoverageValue = 10000,
+                TotalPolicyValue = 100000,
+                Note = "Please Note That",
+                InsuranceID = 2,
+                CoverageID = 2,
+            };
+            pcdmList.Add(pcdm2);
+            return pcdmList;
+        }
+    }
+}
---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Service.WebApi/Models/API/ApiInsurance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQbit.Insurance.Service.WebApi.Models.API
+{
+    public class ApiInsurance
+    {
+        public int ID { get; set; }
+        public int AgentID{get;set;}
+        public int ClientID { get; set; }
+        public DateTime Joining_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public Decimal Total_Value { get; set; }
+
+    }
+}
