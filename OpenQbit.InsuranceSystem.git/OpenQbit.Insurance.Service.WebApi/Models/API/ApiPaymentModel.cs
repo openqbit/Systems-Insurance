a6435946a63be6dd4f3570aa3281ac09a386d585
Appuhamy <chaminduilshan@gmail.com>
2017-05-24 13:01:29 +0530

    03-test

--- OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Service.WebApi/Models/API/ApiPaymentModel.cs
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Service.WebApi/Models/API/ApiPaymentModel.cs
@@ -7,5 +7,11 @@ namespace OpenQbit.Insurance.Service.WebApi.Models.API
 {
     public class ApiPaymentModel
     {
+        public int ID { get; set; }
+        public double Amount { get; set; }
+        public String Date { get; set; }
+
+        public int PolicyCoverageDetailID { get; set; }
+
     }
 }
