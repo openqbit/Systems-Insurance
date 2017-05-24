710ffc414547f096968c1dfb630a7fed21bfae19
sandali_thissera <pst.ames1@gmail.com>
2017-05-24 07:04:49 +0530

    01

---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Common.Models/CoverageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Insurance.Common.Models
+{
+    public class CoverageModel
+    {
+        public int ID { get; set; }
+        public String Conditions { get; set; }
+        public String CoverageType { get; set; }
+        public String Includes { get; set; }
+
+        public virtual PolicyTypeModel PolicyTypes { get; set; }
+        public virtual PolicyCoverageDetailModel PolicyCoverageDetails { get; set; }
+    }
+}
