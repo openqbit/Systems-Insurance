2a2d0f6b15f9aa405b419334122fbe742a7239cc
Pahansith Gunathilake <pahansith.wsg@gmail.com>
2017-06-07 22:50:47 +0530

    Add Repository and Connect Context with Repository

---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.DataAccess.DAL.Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Insurance.DataAccess.DAL.Contracts
+{
+    public interface IRepository
+    {
+        List<T> GetAll<T>() where T : class;
+        T Find<T>(Expression<Func<T, bool>> predicate) where T : class;
+        List<T> FindList<T>(Expression<Func<T, bool>> predicate) where T : class;
+        bool Create<T>(T obj) where T : class;
+        bool Update<T>(T obj) where T : class;
+        bool Delete<T>(T obj) where T : class;
+        bool Save();
+    }
+}
---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.DataAccess.DAL/Repository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Insurance.DataAccess.DAL.Contracts;
+using System.Data.Entity;
+
+namespace OpenQbit.Insurance.DataAccess.DAL
+{
+    class Repository : IRepository
+    {
+        ServiceDBContext _db = new ServiceDBContext();
+        public bool Create<T>(T obj) where T : class
+        {
+            try
+            {
+                _db.Set<T>().Add(obj);
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            
+        }
+
+        public bool Delete<T>(T obj) where T : class
+        {
+            try
+            {
+                _db.Set<T>().Remove(obj);
+                return true;
+            }
+            catch (Exception edb)
+            {
+                return false;
+            }
+        }
+
+        public T Find<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return _db.Set<T>().FirstOrDefault<T>(predicate);
+        }
+
+        public List<T> FindList<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return _db.Set<T>().Where<T>(predicate).ToList();
+        }
+
+        public List<T> GetAll<T>() where T : class
+        {
+            return _db.Set<T>().ToList();
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                _db.SaveChanges();
+                return true;
+            }
+            catch (Exception edb)
+            {
+                return false;
+            }
+        }
+
+        public bool Update<T>(T obj) where T : class
+        {
+            try
+            {
+                var edbstate = _db.Entry(obj);
+                _db.Set<T>().Attach(obj);
+                edbstate.State = EntityState.Modified;
+                return true;
+            }
+            catch (Exception edb)
+            {
+                return false;
+            }
+        }
+    }
+}
