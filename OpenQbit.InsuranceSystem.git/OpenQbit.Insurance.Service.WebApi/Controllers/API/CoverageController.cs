9dfd281cfca26d31dbc900a1651229bfef3b1c45
lilanthads <lilanthads@openqubit.com>
2017-06-14 18:14:23 +0100

    Revert "CoverageController and DocumentController"
    This reverts commit 696cc7f0b06c58654635327091903f02f5200782.

--- OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Service.WebApi/Controllers/API/CoverageController.cs
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Service.WebApi/Controllers/API/CoverageController.cs
@@ -6,73 +6,61 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
-using OpenQbit.Insurance.Common.Ioc;
-using OpenQbit.Insurance.BusinessService.Contracts;
-using Microsoft.Practices.Unity; // InjectionConstructor
 
 namespace OpenQbit.Insurance.Service.WebApi.Controllers.API
 {
     public class CoverageController : ApiController
     {
-        ICoverageManager _coverageManager = UnityResolver.Resolve<ICoverageManager>();
-
         public HttpResponseMessage Post(ApiCoverageModel accident)
         {
-            if (_coverageManager.Recored(accident)) return new HttpResponseMessage(HttpStatusCode.OK);
-            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            return new HttpResponseMessage(HttpStatusCode.OK);
         }
 
         public HttpResponseMessage Put(ApiCoverageModel accident)
         {
-            if (_coverageManager.Update(accident)) return new HttpResponseMessage(HttpStatusCode.OK);
-            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            return new HttpResponseMessage(HttpStatusCode.OK); ;
         }
 
-        public HttpResponseMessage Delete(ApiCoverageModel accident)
+        public HttpResponseMessage Delete(int? ID)
         {
-            if (_coverageManager.Delete(accident)) return new HttpResponseMessage(HttpStatusCode.OK);
-            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            return new HttpResponseMessage(HttpStatusCode.OK); ;
         }
 
         public ApiCoverageModel Get(int? ID)
         {
-            //ApiCoverageModel coverage = new ApiCoverageModel
-            //{
-            //    ID = (int)ID,
-            //    CoverageType = "Fire Coverage",
-            //    Includes = "Car,Furnitures,Grand Piano",
-            //    conditions= "Fire insurance covers a policyholder against fire loss or damage brought about by the ignition of fire, electricity, lightning or explosion of gas, natural disasters, and bursting and overflowing of a water tank or pipes.",
-            //};
-            //return coverage;
-
-            return _coverageManager.Find<ApiCoverageModel>(e => e.ID == agent.ID);
+            ApiCoverageModel coverage = new ApiCoverageModel
+            {
+                ID = (int)ID,
+                CoverageType = "Fire Coverage",
+                Includes = "Car,Furnitures,Grand Piano",
+                conditions= "Fire insurance covers a policyholder against fire loss or damage brought about by the ignition of fire, electricity, lightning or explosion of gas, natural disasters, and bursting and overflowing of a water tank or pipes.",
+            };
+            return coverage;
         }
 
         public List<ApiCoverageModel> GetList()
         {
-            //List<ApiCoverageModel> CoverageList = new List<ApiCoverageModel>();
+            List<ApiCoverageModel> CoverageList = new List<ApiCoverageModel>();
 
-            //ApiCoverageModel coverage1 = new ApiCoverageModel
-            //{
-            //    ID = 1,
-            //    CoverageType = "Fire Coverage",
-            //    Includes = "Car,Furnitures,Grand Piano",
-            //    conditions = "Fire insurance covers a policyholder against fire loss or damage brought about by the ignition of fire, electricity, lightning or explosion of gas, natural disasters, and bursting and overflowing of a water tank or pipes.",
-            //};
-            //CoverageList.Add(coverage1);
+            ApiCoverageModel coverage1 = new ApiCoverageModel
+            {
+                ID = 1,
+                CoverageType = "Fire Coverage",
+                Includes = "Car,Furnitures,Grand Piano",
+                conditions = "Fire insurance covers a policyholder against fire loss or damage brought about by the ignition of fire, electricity, lightning or explosion of gas, natural disasters, and bursting and overflowing of a water tank or pipes.",
+            };
+            CoverageList.Add(coverage1);
 
-                //ApiCoverageModel coverage2 = new ApiCoverageModel
-                //{
-                //    ID = 2,
-                //    CoverageType = "Fire Coverage",
-                //    Includes = "Car,Furnitures,House",
-                //    conditions = "Fire insurance covers a policyholder against fire loss or damage brought about by the ignition of fire, electricity, lightning or explosion of gas, natural disasters, and bursting and overflowing of a water tank or pipes.",
-                //};
-                //CoverageList.Add(coverage2);
+            ApiCoverageModel coverage2 = new ApiCoverageModel
+            {
+                ID = 2,
+                CoverageType = "Fire Coverage",
+                Includes = "Car,Furnitures,House",
+                conditions = "Fire insurance covers a policyholder against fire loss or damage brought about by the ignition of fire, electricity, lightning or explosion of gas, natural disasters, and bursting and overflowing of a water tank or pipes.",
+            };
+            CoverageList.Add(coverage2);
 
-                //return CoverageList;
-
-            return _coverageManager.GetAll<ApiCoverageModel>();
+            return CoverageList;
         }
 
 
