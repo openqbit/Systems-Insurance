800f746056afb28a3c72ec8061b1ebf44c34ad49
sandali_thissera <pst.ames1@gmail.com>
2017-06-14 14:37:16 +0530

    Endorsement and Insurance Controller

--- OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Service.WebApi/Controllers/API/InsuranceController.cs
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Service.WebApi/Controllers/API/InsuranceController.cs
@@ -1,69 +1,82 @@
-﻿using System;
+﻿using OpenQbit.Insurance.Service.WebApi.Models.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using OpenQbit.Insurance.Service.WebApi.Models.API;
+using OpenQbit.Insurance.Common.Ioc;
+using OpenQbit.Insurance.BusinessService.Contracts;
+using Microsoft.Practices.Unity;
 
 namespace OpenQbit.Insurance.Service.WebApi.Controllers.API
 {
     public class InsuranceController : ApiController
     {
+        IInsuranceManager _insuranceManager = UnityResolver.Resolve<IInsuranceManager>();
+
         public HttpResponseMessage Post(ApiInsuranceModel accident)
         {
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            if (_insuranceManager.Recored(accident)) return new HttpResponseMessage(HttpStatusCode.OK);
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
         }
 
         public HttpResponseMessage Put(ApiInsuranceModel accident)
         {
-            return new HttpResponseMessage(HttpStatusCode.OK); ;
+            if (_insuranceManager.Update(accident)) return new HttpResponseMessage(HttpStatusCode.OK);
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
         }
 
-        public HttpResponseMessage Delete(int? ID)
+        public HttpResponseMessage Delete(ApiInsuranceModel accident)
         {
-            return new HttpResponseMessage(HttpStatusCode.OK); ;
+            if (_insuranceManager.Delete(accident)) return new HttpResponseMessage(HttpStatusCode.OK);
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
         }
 
-        public ApiInsuranceModel Get(int? ID)
+        public ApiInsuranceModel Get(ApiInsuranceModel accident)
         {
-            ApiInsuranceModel insurance = new ApiInsuranceModel
-            {
-                ID = (int)ID,
-                AgentID = 1,
-                ClientID = 1,
-                Joining_Date = new DateTime(2000, 02, 29),
-                End_Date = new DateTime(2017, 02, 29),
-                Total_Value = 1000000,
-            };
+            /* ApiInsuranceModel insurance = new ApiInsuranceModel
+             {
+                 //ID = (int)ID,
+                 //AgentID = 1,
+                 //ClientID = 1,
+                 //Joining_Date = new DateTime(2000, 02, 29),
+                 //End_Date = new DateTime(2017, 02, 29),
+                 //Total_Value = 1000000,
+             };
 
-            return insurance;
+             return insurance;*/
+
+            return _insuranceManager.Find<ApiInsuranceModel>(e => e.ID == accident.ID);
         }
 
         public List<ApiInsuranceModel> GetList()
         {
-            List<ApiInsuranceModel> InsuranceList = new List<ApiInsuranceModel>();
+            /*List<ApiInsuranceModel> InsuranceList = new List<ApiInsuranceModel>();
             ApiInsuranceModel insurance1 = new ApiInsuranceModel
             {
-                ID = 1,
-                AgentID = 1,
-                ClientID = 1,
-                Joining_Date = new DateTime(2000, 02, 29),
-                End_Date = new DateTime(2017, 02, 29),
-                Total_Value = 1000000,
+                //ID = 1,
+                //AgentID = 1,
+                //ClientID = 1,
+                //Joining_Date = new DateTime(2000, 02, 29),
+                //End_Date = new DateTime(2017, 02, 29),
+                //Total_Value = 1000000,
             };
             InsuranceList.Add(insurance1);
             ApiInsuranceModel insurance2 = new ApiInsuranceModel
             {
-                ID = 2,
-                AgentID = 1,
-                ClientID = 1,
-                Joining_Date = new DateTime(2010, 02, 29),
-                End_Date = new DateTime(2020, 02, 29),
-                Total_Value = 1000000,
+                //ID = 2,
+                //AgentID = 1,
+                //ClientID = 1,
+                //Joining_Date = new DateTime(2010, 02, 29),
+                //End_Date = new DateTime(2020, 02, 29),
+                //Total_Value = 1000000,
             };
             InsuranceList.Add(insurance2);
-            return InsuranceList;
+            return InsuranceList;*/
+
+            return _insuranceManager.GetAll<ApiInsuranceModel>();
         }
     }
 }
