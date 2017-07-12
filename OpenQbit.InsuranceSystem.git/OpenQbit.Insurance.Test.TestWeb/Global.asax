6325fe765bc7c03e225e3cf755f4a7b131bd1cad
Pahansith Gunathilake <pahansith.wsg@gmail.com>
2017-07-12 07:45:29 +0530

    TestWeb project Added

---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Test.TestWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenQbit.Insurance.Test.TestWeb.MvcApplication" Language="C#" %>
---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Test.TestWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenQbit.Insurance.Test.TestWeb.Startup))]
+namespace OpenQbit.Insurance.Test.TestWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
