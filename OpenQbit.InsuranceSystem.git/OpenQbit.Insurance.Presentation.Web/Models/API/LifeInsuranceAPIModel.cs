c1c0802c5c5cc8794e5ecc2485802a1d124934cc
harinduliyanage <harindu.sul@gmail.com>
2017-07-19 00:52:34 -0700

    Create API Life Insurance Model, API Life Insurance Beneficiary Model,API Marine Insurance,API Motor Insurance Model,API Fire Insurance Model for Presentation

---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Presentation.Web/Models/API/LifeInsuranceAPIModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OpenQbit.Insurance.Presentation.Web.Models.API.Contracts;
+
+
+namespace OpenQbit.Insurance.Presentation.Web.Models.API
+{
+    public class LifeInsuranceAPIModel : IApiInsuranceTypeModel
+    {
+        public enum TOBBACO_TYPES
+        {
+            CIGARETTES, CIGARS, OTHERS
+        }
+        public String Occupation { get; set; }
+        public double AnnualIncome { get; set; }
+        public double NetWorth { get; set; }
+        public bool TobbacoUsingStatus { get; set; }
+        public TOBBACO_TYPES? TobbacoTypesUsing { get; set; }
+        public ApiLifeInsuranceBeneficiaryModel Beneficiary { get; set; }
+    }
+}
