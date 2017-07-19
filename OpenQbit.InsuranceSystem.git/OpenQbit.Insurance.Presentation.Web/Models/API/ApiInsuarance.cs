520c26555aab68c3f4c4ac07a389e3f86362fdbd
sandali_thissera <pst.ames1@gmail.com>
2017-07-19 12:21:40 +0530

    Delete Previouse API Models in Presentation Layer

---
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Presentation.Web/Models/API/ApiInsuarance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQbit.Insurance.Service.WebApi.Models.API
+{
+    public class ApiInsuranceModel
+    {
+        public enum InsuranceTypes
+        {
+            LIFE_INSURANCE,
+            RE_INSURANCE,
+            MOTOR_INSURANCE,
+            MARINE_INSURANCE,
+            FIRE_INSURANCE
+        }
+        public int ID { get; set; }
+        public DateTime Joining_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public Decimal Total_Value { get; set; }
+
+        public InsuranceTypes InsuranceType { get; set; }
+        public IApiInsuranceTypeModel SelectedInsurance { get; set; }
+        public int AgentID { get; set; }
+
+        public ApiClientModel Client { get; set; }
+        public ApiDocumentModel Documents { get; set; }
+        public ApiPolicyCoverageDetailModel PolicyDetails { get; set; }
+        public ApiCoverageModel Coverage { get; set; }
+    }
+}
