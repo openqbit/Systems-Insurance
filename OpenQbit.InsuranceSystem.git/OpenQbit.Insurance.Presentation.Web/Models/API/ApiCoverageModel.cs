ac138aaa2835eea2d7ba106c29ae1974ed480d83
Appuhamy <chaminduilshan@gmail.com>
2017-07-19 16:03:31 +0530

    Error Fixing on Presentation.web.model.contracts

--- OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Presentation.Web/Models/API/ApiCoverageModel.cs
+++ OpenQbit.InsuranceSystem.git/OpenQbit.Insurance.Presentation.Web/Models/API/ApiCoverageModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace OpenQbit.Insurance.Service.WebApi.Models.API
+namespace OpenQbit.Insurance.Presentation.Web.Models.API
 {
     public class ApiCoverageModel
     {
